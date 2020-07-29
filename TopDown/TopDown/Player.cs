68915f00a76f20c58252aee77f1b72c23a716dc9
Nicholas Raston <270021692@acstudent.ac.nz>
2020-07-30 11:03:20 +1200

    commit

--- TopDown/TopDown/Player.cs
+++ TopDown/TopDown/Player.cs
@@ -24,52 +24,129 @@ namespace TopDown
 
         public void Act()
         {
+            log = "";
 
-        }
+            if (Game.Instance.info.Key == ConsoleKey.I) {
+                UsePotion();
+            }
 
-        public void Log()
-        {
+            Vector2 direction = GetInputDirection();
 
+            if (IsObstructed(direction)) {
+                Position += direction;
+            }
         }
 
         private bool CollideCactus()
         {
+            log += "\nYou walk into a cactus & lose 2hp lol.";
+            hp -= 2;
+            if (hp <0) {
+                hp = 0;
+            }
             return false;
         }
 
         private bool CollideEnemy()
         {
-            return false;
+            log += "\n An enemy blocks your way.";
+            return true;
         }
 
         private bool CollidePotion(ref char _c)
         {
+            log += "\nYou find a potion.";
+            potions++;
+            _c = Game.Instance.GetRandomGroundChar();
             return false;
         }
 
         private bool CollideRock()
         {
-            return false;
+            log += "\nYou've been rock-blocked";
+            return true;
+        }
+
+        private Vector2 GetInputDirection() {
+            if (Game.Instance.info.Key == ConsoleKey.W) return Vector2.Down;
+            if (Game.Instance.info.Key == ConsoleKey.S) return Vector2.Up;
+            if (Game.Instance.info.Key == ConsoleKey.A) return Vector2.Left;
+            if (Game.Instance.info.Key == ConsoleKey.D) return Vector2.Right;
+
+            return Vector2.Zero;
         }
 
         private bool HandleObstruction(ref char _c)
         {
-            return false;
+            switch (_c) {
+                case '▲':
+                    return CollideRock();
+                case '♣':
+                    return CollideCactus();
+                case '§':
+                    return CollideEnemy();
+                case 'Φ':
+                    return CollidePotion(ref _c);
+                default:
+                    return true;
+            }
+            
         }
 
-        private bool IsObstructed(Vector2 _direction)
-        {
-            return false;
+        private bool IsObstructed(Vector2 _direction) {
+            Vector2 nextPosition = Position + _direction;
+
+            if (nextPosition.x > Game.Instance.map.GetLength(0) - 1
+            || nextPosition.y > Game.Instance.map.GetLength(1) - 1
+            || nextPosition.x < 0
+            || nextPosition.y < 0
+            ) {
+                log += "\nYou shall not pass!";
+                return true;
+            }
+
+            string terrainChars = " \".,'`\'";
+            bool isObjectTile = false;
+            foreach (char c in terrainChars) {
+                if (!terrainChars.Contains(Game.Instance.map[nextPosition.x, nextPosition.y].ToString())) {
+                    isObjectTile = true;
+                }
+            }
+
+            if (isObjectTile) {
+                isObjectTile = HandleObstruction(ref Game.Instance.map[nextPosition.x, nextPosition.y]);
+            }
+            return isObjectTile;
         }
 
-        private Vector2 GetInputDirection()
-        {
-            return Vector2.Zero;
+
+        public void Log() {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("hpL " + hp + "\t\tpotions: " + potions);
+            Console.ResetColor();
+            Console.Write("\nWASD = Move\tI=Use Potion");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("\npos: ()" + Position.ToString());
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write(log);
         }
 
         private void UsePotion()
         {
+            if (potions > 0) {
+                if (hp < maxHP) {
+                    log = "\nYou used a potion, recovering 5hp.";
+                    potions--;
+                    hp += 5;
 
+                    if (hp > maxHP) {
+                        hp = maxHP;
+                    }
+                } else {
+                    log = "\nYou don't have any potions";
+                }
+            }
         }
 
     }
